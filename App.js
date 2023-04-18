f4210ec6f52e8dcc51c39eb23830a2035f99d008
Erivaldo de Jesus <rivaldo18@gmail.com>
2023-04-18 03:43:37 -0300

    "Vai"

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ainda me sinto perdido nos estudos</Text>
+      <Text>Ainda me sinto perdido nos estudos!! Complicado</Text>
       <StatusBar style="auto" />
     </View>
   );
